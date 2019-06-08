8f07fbc9306c3ff8870fbec8b1753266fb1faf4b
dantullis <dantullis@dantulliss-MacBook-Pro.local>
2019-06-07 21:58:44 -0400

    add dependencies and begin api call code

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-module.exports = function removeSpacesFromString(string) {
-  if (typeof string !== "string") throw new TypeError("Must be a string!");
-  return string.replace(/\s/g, "");
+'use strict';
+const ky = require('ky-universal');
+
+function OvalCMS(client_token) {
+  if (typeof client_token !== "string") {
+    throw new TypeError("Must be a string!");
+  }
+
+  return await ky.get('http://www.ovalcms.com/api/content/' + client_token).json();
 };
+
+module.exports = OvalCMS;
